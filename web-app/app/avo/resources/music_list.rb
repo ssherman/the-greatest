171c714bedd5c3e8fffe283abfb4cab83d5eef7d
Shane Sherman <shane.sherman@gmail.com>
2025-07-22 18:16:45 -0500

    cleaned up avo list controllers to support STI

--- web-app/app/avo/resources/music_list.rb
+++ web-app/app/avo/resources/music_list.rb
@@ -1,18 +1,20 @@
-class Avo::Resources::List < Avo::BaseResource
+class Avo::Resources::MusicList < Avo::BaseResource
   # self.includes = []
   # self.attachments = []
+  self.model_class = ::Music::List
+  self.title = :name
   # self.search = {
   #   query: -> { query.ransack(id_eq: params[:q], m: "or").result(distinct: false) }
   # }
 
   def fields
     field :id, as: :id
-    field :type, as: :text
+    field :type, as: :text, readonly: true
     field :name, as: :text
     field :description, as: :textarea
     field :source, as: :text
     field :url, as: :text
-    field :status, as: :select, enum: ::List.statuses
+    field :status, as: :select, enum: ::Music::List.statuses
     field :estimated_quality, as: :number
     field :high_quality_source, as: :boolean
     field :category_specific, as: :boolean
@@ -24,5 +26,11 @@ class Avo::Resources::List < Avo::BaseResource
     field :voter_names_unknown, as: :boolean
     field :formatted_text, as: :textarea
     field :raw_html, as: :textarea
+    field :submitted_by_id, as: :number
+    field :list_items, as: :has_many
+    field :submitted_by, as: :belongs_to
+    field :list_penalties, as: :has_many
+    field :penalties, as: :has_many, through: :list_penalties
+    field :ai_chats, as: :has_many
   end
 end
