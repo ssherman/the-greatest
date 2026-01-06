378f571ddaa19c4925fe1917cd18a8c98ab1f862
Shane Sherman <shane.sherman@gmail.com>
2026-01-05 22:37:51 -0600

    anon lists create

--- web-app/app/controllers/music/lists_controller.rb
+++ web-app/app/controllers/music/lists_controller.rb
@@ -1,7 +1,7 @@
 class Music::ListsController < ApplicationController
   layout "music/application"
 
-  before_action :load_ranking_configurations
+  before_action :load_ranking_configurations, only: [:index]
 
   def index
     @albums_ranked_lists = @albums_ranking_configuration.ranked_lists
@@ -19,10 +19,63 @@ class Music::ListsController < ApplicationController
       .limit(50)
   end
 
+  def new
+    @list = List.new
+  end
+
+  def create
+    list_class = list_class_from_type(params[:list_type])
+
+    if list_class.nil?
+      @list = List.new(list_params)
+      @list.errors.add(:base, "Please select a list type (Album List or Song List)")
+      render :new, status: :unprocessable_entity
+      return
+    end
+
+    @list = list_class.new(list_params)
+    @list.status = :unapproved
+    @list.submitted_by = current_user if current_user
+
+    if @list.save
+      redirect_to music_lists_path, notice: "Thank you for your submission! Your list will be reviewed shortly."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def load_ranking_configurations
     @albums_ranking_configuration = Music::Albums::RankingConfiguration.default_primary
     @songs_ranking_configuration = Music::Songs::RankingConfiguration.default_primary
+  end
+
+  def list_class_from_type(list_type)
+    case list_type
+    when "albums"
+      Music::Albums::List
+    when "songs"
+      Music::Songs::List
+    end
+  end
+
+  def list_params
+    params.require(:list).permit(
+      :name,
+      :description,
+      :source,
+      :url,
+      :year_published,
+      :number_of_voters,
+      :num_years_covered,
+      :location_specific,
+      :category_specific,
+      :yearly_award,
+      :voter_count_estimated,
+      :voter_names_unknown,
+      :voter_count_unknown,
+      :raw_html
+    )
   end
 end
--- web-app/test/controllers/music/lists_controller_test.rb
+++ web-app/test/controllers/music/lists_controller_test.rb
@@ -4,6 +4,7 @@ module Music
   class ListsControllerTest < ActionDispatch::IntegrationTest
     setup do
       host! "dev.thegreatestmusic.org"
+      @user = users(:regular_user)
     end
 
     test "should get index" do
@@ -16,6 +17,159 @@ module Music
       assert_response :success
       assert_select "h2", text: "Top Album Lists"
       assert_select "h2", text: "Top Song Lists"
+    end
+
+    test "index should have submit a list link" do
+      get "/lists"
+      assert_response :success
+      assert_select "a[href=?]", "/lists/new"
+    end
+
+    test "should get new" do
+      get "/lists/new"
+      assert_response :success
+    end
+
+    test "new should render submission form" do
+      get "/lists/new"
+      assert_response :success
+      assert_select "h1", text: "Submit a Music List"
+    end
+
+    test "should create album list as anonymous user" do
+      assert_difference("Music::Albums::List.count", 1) do
+        post "/lists", params: {
+          list_type: "albums",
+          list: {
+            name: "Test Album List",
+            description: "A test list",
+            source: "Test Source"
+          }
+        }
+      end
+
+      list = Music::Albums::List.last
+      assert_equal "Test Album List", list.name
+      assert_equal "unapproved", list.status
+      assert_nil list.submitted_by_id
+
+      assert_redirected_to music_lists_path
+      assert_equal "Thank you for your submission! Your list will be reviewed shortly.", flash[:notice]
+    end
+
+    test "should create song list as anonymous user" do
+      assert_difference("Music::Songs::List.count", 1) do
+        post "/lists", params: {
+          list_type: "songs",
+          list: {
+            name: "Test Song List",
+            source: "Test Source"
+          }
+        }
+      end
+
+      list = Music::Songs::List.last
+      assert_equal "Test Song List", list.name
+      assert_equal "unapproved", list.status
+      assert_nil list.submitted_by_id
+
+      assert_redirected_to music_lists_path
+    end
+
+    test "should create list with logged in user setting submitted_by_id" do
+      sign_in_as(@user, stub_auth: true)
+
+      assert_difference("Music::Albums::List.count", 1) do
+        post "/lists", params: {
+          list_type: "albums",
+          list: {
+            name: "User Submitted List"
+          }
+        }
+      end
+
+      list = Music::Albums::List.last
+      assert_equal @user.id, list.submitted_by_id
+      assert_redirected_to music_lists_path
+    end
+
+    test "should fail without list_type" do
+      assert_no_difference("List.count") do
+        post "/lists", params: {
+          list: {
+            name: "Test List"
+          }
+        }
+      end
+
+      assert_response :unprocessable_entity
+    end
+
+    test "should fail with missing required name" do
+      assert_no_difference("List.count") do
+        post "/lists", params: {
+          list_type: "albums",
+          list: {
+            description: "A list without a name"
+          }
+        }
+      end
+
+      assert_response :unprocessable_entity
+    end
+
+    test "should fail with invalid url format" do
+      assert_no_difference("List.count") do
+        post "/lists", params: {
+          list_type: "albums",
+          list: {
+            name: "Test List",
+            url: "not-a-valid-url"
+          }
+        }
+      end
+
+      assert_response :unprocessable_entity
+    end
+
+    test "should accept all list attributes" do
+      assert_difference("Music::Albums::List.count", 1) do
+        post "/lists", params: {
+          list_type: "albums",
+          list: {
+            name: "Complete List",
+            description: "Full description",
+            source: "Rolling Stone",
+            url: "https://example.com/list",
+            year_published: 2024,
+            number_of_voters: 300,
+            num_years_covered: 50,
+            location_specific: true,
+            category_specific: false,
+            yearly_award: true,
+            voter_count_estimated: true,
+            voter_names_unknown: false,
+            voter_count_unknown: false,
+            raw_html: "1. Test Album - Test Artist\n2. Another Album - Another Artist"
+          }
+        }
+      end
+
+      list = Music::Albums::List.last
+      assert_equal "Complete List", list.name
+      assert_equal "Full description", list.description
+      assert_equal "Rolling Stone", list.source
+      assert_equal "https://example.com/list", list.url
+      assert_equal 2024, list.year_published
+      assert_equal 300, list.number_of_voters
+      assert_equal 50, list.num_years_covered
+      assert list.location_specific
+      assert_not list.category_specific
+      assert list.yearly_award
+      assert list.voter_count_estimated
+      assert_not list.voter_names_unknown
+      assert_not list.voter_count_unknown
+      assert_includes list.raw_html, "Test Album"
     end
   end
 end
