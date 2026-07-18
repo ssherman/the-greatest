ba0872b1767e52f18356a6199644ae922f93016d
Shane Sherman <shane.sherman@gmail.com>
2026-07-17 22:16:04 -0500

    Add inline series books + make_representative (inc 5b task 6)

---
+++ web-app/app/controllers/admin/books/series_books_controller.rb
@@ -0,0 +1,83 @@
+class Admin::Books::SeriesBooksController < Admin::Books::BaseController
+  before_action :set_series_book, only: [:update, :destroy, :make_representative]
+
+  def create
+    @series = ::Books::Series.find(params[:series_id])
+    authorize @series, :update?, policy_class: ::Books::SeriesPolicy
+    @series_book = @series.series_books.build(series_book_params)
+
+    if @series_book.save
+      respond_to do |format|
+        format.turbo_stream { render_series_books("Book added to series.") }
+        format.html { redirect_to admin_books_series_path(@series), notice: "Book added to series." }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { render_association_error(@series_book) }
+        format.html { redirect_to admin_books_series_path(@series), alert: @series_book.errors.full_messages.join(", ") }
+      end
+    end
+  end
+
+  def update
+    @series = @series_book.series
+    authorize @series, :update?, policy_class: ::Books::SeriesPolicy
+
+    if @series_book.update(series_book_params)
+      respond_to do |format|
+        format.turbo_stream { render_series_books("Series book updated.") }
+        format.html { redirect_to admin_books_series_path(@series), notice: "Series book updated." }
+      end
+    else
+      respond_to do |format|
+        format.turbo_stream { render_association_error(@series_book) }
+        format.html { redirect_to admin_books_series_path(@series), alert: @series_book.errors.full_messages.join(", ") }
+      end
+    end
+  end
+
+  def destroy
+    @series = @series_book.series
+    authorize @series, :update?, policy_class: ::Books::SeriesPolicy
+    @series_book.destroy!
+
+    respond_to do |format|
+      format.turbo_stream { render_series_books("Book removed from series.") }
+      format.html { redirect_to admin_books_series_path(@series), notice: "Book removed from series." }
+    end
+  end
+
+  def make_representative
+    @series = @series_book.series
+    authorize @series, :update?, policy_class: ::Books::SeriesPolicy
+    @series.update!(representative_book_id: @series_book.book_id)
+
+    respond_to do |format|
+      format.turbo_stream { render_series_books("Representative book updated.") }
+      format.html { redirect_to admin_books_series_path(@series), notice: "Representative book updated." }
+    end
+  end
+
+  private
+
+  def set_series_book
+    @series_book = ::Books::SeriesBook.find(params[:id])
+  end
+
+  def series_book_params
+    params.require(:books_series_book).permit(:book_id, :position, :numbered, :position_label)
+  end
+
+  def render_series_books(notice)
+    render turbo_stream: [
+      turbo_stream.replace("flash", partial: "admin/shared/flash", locals: {flash: {notice: notice}}),
+      turbo_stream.replace("series_books_list", partial: "admin/books/series/series_books_list", locals: {series: @series})
+    ]
+  end
+
+  def render_association_error(record)
+    render turbo_stream: turbo_stream.replace(
+      "flash", partial: "admin/shared/flash", locals: {flash: {error: record.errors.full_messages.join(", ")}}
+    ), status: :unprocessable_entity
+  end
+end
---
+++ web-app/test/controllers/admin/books/series_books_controller_test.rb
@@ -0,0 +1,76 @@
+require "test_helper"
+
+module Admin
+  module Books
+    class SeriesBooksControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        @admin_user = users(:admin_user)
+        @regular_user = users(:regular_user)
+        @series = books_series(:asoiaf)
+        host! Rails.application.config.domains[:books]
+      end
+
+      test "create adds a series book and redirects" do
+        sign_in_as(@admin_user, stub_auth: true)
+        book = books_books(:war_and_peace)
+        assert_difference("@series.series_books.count", 1) do
+          post admin_books_series_series_books_path(@series), params: {books_series_book: {book_id: book.id, position: "3", numbered: "1", position_label: "Book 3"}}
+        end
+        assert_redirected_to admin_books_series_path(@series)
+        assert_equal book.id, @series.series_books.order(:created_at).last.book_id
+      end
+
+      test "create rejects a duplicate book in the series" do
+        sign_in_as(@admin_user, stub_auth: true)
+        existing = @series.series_books.first.book
+        assert_no_difference("::Books::SeriesBook.count") do
+          post admin_books_series_series_books_path(@series), params: {books_series_book: {book_id: existing.id, position: "9"}}
+        end
+        assert_redirected_to admin_books_series_path(@series)
+      end
+
+      test "create is forbidden for a regular user" do
+        sign_in_as(@regular_user, stub_auth: true)
+        book = books_books(:war_and_peace)
+        assert_no_difference("::Books::SeriesBook.count") do
+          post admin_books_series_series_books_path(@series), params: {books_series_book: {book_id: book.id, position: "3"}}
+        end
+        assert_redirected_to books_root_path
+      end
+
+      test "update changes the position" do
+        sign_in_as(@admin_user, stub_auth: true)
+        sb = books_series_books(:asoiaf_got)
+        patch admin_books_series_book_path(sb), params: {books_series_book: {position: "5", numbered: "0", position_label: "Prequel"}}
+        assert_redirected_to admin_books_series_path(@series)
+        assert_equal 5.0, sb.reload.position
+        assert_equal false, sb.numbered
+      end
+
+      test "destroy removes the series book" do
+        sign_in_as(@admin_user, stub_auth: true)
+        sb = @series.series_books.create!(book: books_books(:war_and_peace), position: 4)
+        assert_difference("::Books::SeriesBook.count", -1) do
+          delete admin_books_series_book_path(sb)
+        end
+        assert_redirected_to admin_books_series_path(@series)
+      end
+
+      test "make_representative sets the series representative_book" do
+        sign_in_as(@admin_user, stub_auth: true)
+        sb = books_series_books(:asoiaf_clash)
+        post make_representative_admin_books_series_book_path(sb)
+        assert_redirected_to admin_books_series_path(@series)
+        assert_equal sb.book_id, @series.reload.representative_book_id
+      end
+
+      test "make_representative is forbidden for a regular user" do
+        sign_in_as(@regular_user, stub_auth: true)
+        sb = books_series_books(:asoiaf_clash)
+        post make_representative_admin_books_series_book_path(sb)
+        assert_redirected_to books_root_path
+        assert_nil @series.reload.representative_book_id
+      end
+    end
+  end
+end
